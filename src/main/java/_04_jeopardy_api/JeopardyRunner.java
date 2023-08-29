42d4818f1dff0275085b5f525499052b265cecca
Jetsapon <Jetsapon@192.168.254.21>
2023-08-28 18:31:10 -0700

    JeopardyRunner.java, JeopardyApi.java

--- src/main/java/_04_jeopardy_api/JeopardyRunner.java
+++ src/main/java/_04_jeopardy_api/JeopardyRunner.java
@@ -9,13 +9,24 @@ public class JeopardyRunner {
     public static void main(String[] args) {
 
         JeopardyApi jeopardyApi = new JeopardyApi();
+        Clue clue = new Clue();
 
         //1. Create a score variable
-
+        int score;
+        
         //2. Add a for loop where:
         //i starts at 100,
         //continues while i <= 1000
         //increments by 100
+        for(int i = 100; i<= 1000; i+=100) {
+        	
+        	if(i ==700 || i ==900) {
+        		jeopardyApi.getClue(i);
+        		String question = 
+        	}
+        	
+        	
+        }
 
             //3. If i == 700 or i == 900, continue;
             //there are no questions for these values
